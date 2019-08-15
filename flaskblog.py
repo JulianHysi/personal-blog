af2cfec32936150564ef858231b1e78201f05cca
julian97 <julian_hysi@yahoo.com>
2019-08-15 22:07:34 +0200

    Added templates of html and css

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Julian Hysi',
+        'title': 'First blog post',
+        'content': 'First blog post contents',
+        'date_posted': 'August 2019, 15 (?)'
+    },
+    {
+        'author': 'Albor Hysi',
+        'title': 'Second blog post',
+        'content': 'Second blog post contents',
+        'date_posted': 'August 2019, 15 (defo)'
+    }
+
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
