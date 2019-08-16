1e50255ef9b8ef837fa49c387393dfbf46d98ddb
julian97 <julian_hysi@yahoo.com>
2019-08-16 23:06:09 +0200

    Added LogIn and Registration forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0ab581061e7206cf1055587676133539' #make it an env variable later on
 
 posts = [
     {
@@ -25,6 +29,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account registered for user: {form.username.data}', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'julian@live.com':
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Please enter the right credentials..', 'danger')    
+    return render_template('login.html', title='Log In', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
