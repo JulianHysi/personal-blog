55d58315d22382c9a6fb774cf6d8a6927330a021
julian97 <julian_hysi@yahoo.com>
2019-08-20 22:33:17 +0200

    Switched from hard-coded variable values to environment variables

--- personal_blog/__init__.py
+++ personal_blog/__init__.py
@@ -1,10 +1,11 @@
+from os import environ
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '0ab581061e7206cf1055587676133539' #make it an env variable later on
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY') #environment variable
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')  #environment variable
 
 db = SQLAlchemy(app)
 
