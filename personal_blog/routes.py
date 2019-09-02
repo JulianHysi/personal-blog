acfd6e4e1d78b1193c9a9680d65d6b11ff2d63bd
julian97 <julian_hysi@yahoo.com>
2019-09-02 21:13:55 +0200

    Added pagination to home page

--- personal_blog/routes.py
+++ personal_blog/routes.py
@@ -12,7 +12,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 @app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int) #url query parameter
-    posts = Post.query.paginate(page=page, per_page=2)
+    posts = Post.query.paginate(page=page, per_page=4)
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
