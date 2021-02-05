9b566926e86f3356293b3cdf92329e0fac98198d
Julian Hysi <julian_hysi@yahoo.com>
2021-02-05 18:41:21 +0100

    Add code snippet option to CKEditor

--- personal_blog/__init__.py
+++ personal_blog/__init__.py
@@ -7,15 +7,18 @@ from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = environ.get('SECRET_KEY') #environment variable
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')  #environment variable
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')  # environment variable
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CKEDITOR_SERVE_LOCAL'] = True
 app.config['CKEDITOR_PKG_TYPE'] = 'standard'
+app.config['CKEDITOR_ENABLE_CODESNIPPET'] = True
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 ckeditor = CKEditor(app)
 login_manager = LoginManager(app)
+
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'  # bootstrap class
 
