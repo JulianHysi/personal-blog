bcacac3db07ac32b3d813184e482b75ec1109278
Julian Hysi <julian_hysi@yahoo.com>
2021-02-12 20:55:33 +0100

    Add module docstring to run.py

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
+"""
+A module used to run the application, if called directly
+
+---
+
+Examples
+--------
+    Change directory to the app's top level directory, and hit the
+    following command on your terminal to run the application:
+
+        $ python run.py
+
+Attributes
+----------
+app : FlaskApp
+    the current application instance
+"""
+
 from personal_blog import create_app
 
 app = create_app()
