ed9bd85b720e2f17c913656c9110db6850bef373
Julian Hysi <julian_hysi@yahoo.com>
2021-02-17 14:26:18 +0100

    Add route to update book

--- personal_blog/books/routes.py
+++ personal_blog/books/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, url_for, flash, redirect, abort
+from flask import Blueprint, render_template, url_for, flash, redirect, abort,\
+        request
 from flask_login import current_user, login_required
 
 from personal_blog import db
@@ -32,10 +33,38 @@ def all_books():
     books = Book.query.all()
     return render_template('all_books.html', books=books,
                            sidebar_posts=get_sidebar_posts())
-    
+
 
 @books.route("/book/<int:book_id>")
 def book(book_id):
     book = Book.query.get_or_404(book_id)
     return render_template('book.html', title=book.title, book=book,
                            sidebar_posts=get_sidebar_posts())
+
+
+@books.route("/book/<int:book_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_book(book_id):
+    book = Book.query.get_or_404(book_id)
+    if not current_user.is_admin:  # only the admin can update books
+        abort(403)  # forbidden route
+    form = BookForm()
+    form.submit.label.text = 'Update'
+    if form.validate_on_submit():
+        book.title = form.title.data
+        book.authors = form.authors.data
+        book.edition = form.edition.data
+        if form.link.data:
+            book.link = form.link.data
+        book.description = form.description.data
+        db.session.commit()
+        flash('Book has been updated!', 'success')
+        return redirect(url_for('books.book', book_id=book.id))
+    elif request.method == 'GET':
+        form.title.data = book.title
+        form.authors.data = book.authors
+        form.edition.data = book.edition
+        form.link.data = book.link
+        form.description.data = book.description
+    return render_template('add_book.html', title='Update Book',
+                           form=form, legend='Update Book', hide_sidebar=True)
