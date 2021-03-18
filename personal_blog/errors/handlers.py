868f936726fe86e43dedb178cdd5f2a27e0c96b6
Julian Hysi <julian_hysi@yahoo.com>
2021-03-18 17:07:18 +0100

    Add docstrings to errors blueprint

--- personal_blog/errors/handlers.py
+++ personal_blog/errors/handlers.py
@@ -1,3 +1,17 @@
+"""Module containing error handler route functions.
+
+---
+
+Functions
+---------
+error_403(error): return http response
+    the 403 error handler
+error_404(error): return http response
+    the 404 error handler
+error_500(error): return http response
+    the 500 error handler
+"""
+
 from flask import Blueprint, render_template
 
 errors = Blueprint('errors', __name__)
@@ -5,14 +19,29 @@ errors = Blueprint('errors', __name__)
 
 @errors.app_errorhandler(403)
 def error_403(error):
+    """Error 403 handler route function.
+
+    Render the 403 template, and return the 403 code.
+    """
+
     return render_template('errors/403.html'), 403
 
 
 @errors.app_errorhandler(404)
 def error_404(error):
+    """Error 404 handler route function.
+
+    Render the 404 template, and return the 404 code.
+    """
+
     return render_template('errors/404.html'), 404
 
 
 @errors.app_errorhandler(500)
 def error_500(error):
+    """Error 500 handler route function.
+
+    Render the 500 template, and return the 500 code.
+    """
+
     return render_template('errors/500.html'), 500
